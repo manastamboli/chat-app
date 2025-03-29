44bf0a7f3828e723e1454004ee222bd7aa147f8d
Manas Tamboli <manastamboli2@gamail.com>
2025-03-29 11:19:12 +0530

    group chat controllers added

--- backend/src/routes/message.route.js
+++ backend/src/routes/message.route.js
@@ -1,14 +1,15 @@
 import express from "express";
 import { protectRoute } from "../middleware/auth.middleware.js";
-import { getMessages, getUsersForSidebar, sendMessage } from "../controllers/message.controller.js";
+import { getMessages, getUsersForSidebar, sendMessage,createGroup,getMyGroups} from "../controllers/message.controller.js";
 import upload from "../lib/multer.js";
 
 const router = express.Router();
 
 router.get("/users", protectRoute, getUsersForSidebar);
 router.get("/:id", protectRoute, getMessages);
-
+router.get("/my-groups", protectRoute, getMyGroups);
 // Use multer middleware to handle file uploads
 router.post("/send/:id", protectRoute, upload.single('image'), sendMessage);
+router.post("/create-group",protectRoute,createGroup)
 
 export default router;
